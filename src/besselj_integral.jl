5eeb53b0978a0adc483f6a9658b61dad5107c34b
Alexander Samoilov <alexander.samoilov@gmail.com>
2015-11-24 16:17:09 +0000

    fixed LaTeXStrings L"...".

--- src/besselj_integral.jl
+++ src/besselj_integral.jl
@@ -3,15 +3,16 @@ using Gadfly
 using LaTeXStrings # for L"" strings
 
 # WARNING: integrate(f,a,b) is deprecated, use (quadgk(f,a,b))[1] instead
-f(x) = integrate(z -> besselj(1, z), 0.0, x) 
+# f(x) = integrate(z -> besselj(1, z), 0.0, x)
+
+f(x) = quadgk(z -> besselj(1, z), 0.0, x)[1]
 
 my_plot_pgf = plot(f, 0, 100
             , Guide.XLabel("r"
                     , orientation=:horizontal)
-#            , Guide.YLabel(L"\int_{z=0}^{z=\textbf{r}}J_1(z)dz"
-            , Guide.YLabel("\$\\int_{z=0}^{z=\\textbf{r}}J_1(z)dz\$"
+            , Guide.YLabel(bytestring(L"\int_{z=0}^{z=\textbf{r}}J_1(z)dz")
                     , orientation=:vertical))
 
 draw(PGF("images/Int_J1_tex2.tex", 5inch, 3inch), my_plot_pgf)
 
-# see [PGF LaTeX output seems to be broken #753](https://github.com/dcjones/Gadfly.jl/issues/753)
+# see also [PGF LaTeX output seems to be broken #753](https://github.com/dcjones/Gadfly.jl/issues/753)
