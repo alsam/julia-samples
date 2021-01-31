950a70fb1243c49b7c1830c9119bfee20c7e0fae
Alexander Samoilov <alexander.samoilov@gmail.com>
2021-01-31 23:16:37 +0300

    experiments with precompiled sysimage IV.

--- src/sysimage.jl
+++ src/sysimage.jl
@@ -1,3 +1,4 @@
 using PackageCompiler
 create_sysimage(:IterativeSolvers, sysimage_path="sys_iterative_solvers.so", precompile_execution_file="precompile_qp.jl")
+#create_sysimage(:IterativeSolvers, sysimage_path="sys_iterative_solvers.so", precompile_execution_file="custom_sysimage.jl")
 
