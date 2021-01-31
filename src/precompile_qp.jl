62ea133b07d5c1ae9b47f3f5ad353faa2a93be86
Alexander Samoilov <alexander.samoilov@gmail.com>
2021-01-31 19:08:26 +0000

    experiments with precompiled sysimage II.

--- src/precompile_qp.jl
+++ src/precompile_qp.jl
@@ -1,4 +1,9 @@
-resize!(ARGS, 2)
-ARGS[1] = "../benchmarks/3QP/toy1"
-ARGS[2] = "toy1.out"
-include("qp.jl")
+function exec()
+    resize!(ARGS, 2)
+    ARGS[1] = "../benchmarks/3QP/toy1"
+    ARGS[2] = "toy1.out"
+    include("qp.jl")
+end
+
+@time exec()
+@time exec()
